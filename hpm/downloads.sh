c1001f5bc856a0065686f4cc35acb8d2db0eb0a2
Asis Patra <asis.patra0425@gmail.com>
2018-03-08 19:47:52 +0530

    Update downloads.sh

--- hpm/downloads.sh
+++ hpm/downloads.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/asispatra/tools/master/hpm/allmodels.sh
 wget https://raw.githubusercontent.com/asispatra/tools/master/hpm/alexnet.sh
+wget https://raw.githubusercontent.com/asispatra/tools/master/hpm/commits.sh
+wget https://raw.githubusercontent.com/asispatra/tools/master/hpm/hpms.sh
