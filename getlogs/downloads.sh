97a0ff84d06c1dddb229eed934159fd39e5f430a
Asis Patra <asis.patra0425@gmail.com>
2018-03-15 12:09:18 +0530

    Create downloads.sh

---
+++ getlogs/downloads.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/asispatra/tools/master/getlogs/README.md
+wget https://raw.githubusercontent.com/asispatra/tools/master/getlogs/Log_PATH
+wget https://raw.githubusercontent.com/asispatra/tools/master/getlogs/startLogging.sh
+wget https://raw.githubusercontent.com/asispatra/tools/master/getlogs/stopLogging.sh
