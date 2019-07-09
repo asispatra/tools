d2a549a22cb7e29c13dd6c2ea9ffe784b92c382e
Asis Patra <asis.patra0425@gmail.com>
2019-07-09 11:30:56 +0530

    Create slideshare.sh

---
+++ slideshare.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+
+if [ $# -ne 1 ] ; then
+  echo "Usage: $0 <SlideShare_URL>"
+  exit 1
+fi
+
+URL="$1"
+#URL="https://www.slideshare.net/HJvanVeen/feature-engineering-72376750"
+DIR=$(echo $URL | sed 's/.*\/\(.*\)$/\1/')
+mkdir -p "$DIR"
+echo "$DIR"
+for IMG in $(curl -s $URL| grep jpg | grep data-full | tr -d ' ' | cut -d '"' -f2) ; do
+  echo $IMG
+  FILENAME=$(echo $IMG | sed 's/.*\/\(.*\.jpg\).*/\1/')
+  FILENAME="$DIR/$FILENAME"
+  curl -s "$IMG" > $FILENAME
+done
