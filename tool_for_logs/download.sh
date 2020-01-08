0db3e0cbd2394cd870e6f9cab3ca6f3e0045bb8e
Asis Patra <asis.patra0425@gmail.com>
2020-01-08 15:56:06 +0530

    Create download.sh

---
+++ tool_for_logs/download.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/asispatra/tools/master/tool_for_logs/Log_PATH
+wget https://raw.githubusercontent.com/asispatra/tools/master/tool_for_logs/README.md
+wget https://raw.githubusercontent.com/asispatra/tools/master/tool_for_logs/gpu.sh
+wget https://raw.githubusercontent.com/asispatra/tools/master/tool_for_logs/startLogging.sh
+wget https://raw.githubusercontent.com/asispatra/tools/master/tool_for_logs/stopLogging.sh
